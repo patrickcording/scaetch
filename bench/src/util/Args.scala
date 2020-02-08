f72269e1ffd7c06f6da3341f106a20ab4d595c4e
Patrick Cording <patrick.cording@datarobot.com>
2020-02-08 20:19:43 +0100

    Remove k from args

--- bench/src/util/Args.scala
+++ bench/src/util/Args.scala
@@ -6,7 +6,6 @@ import scala.util.{Failure, Try}
 case class BenchmarkArgs(
                         depths: List[Int],
                         widths: List[Int],
-                        k: Int,
                         bufferSize: Int,
                         dataType: String,
                         file: String
@@ -17,15 +16,15 @@ object Args {
     s"""
        |Usage:
        |
-       |  mill benchmark.Benchmark depth width_range k buffer_size file
+       |  mill benchmark.Benchmark depth_range width_range buffer_size file
        |
        |Example:
        |
-       |  mill benchmark.Benchmark 5,10 16,512 50 1000 string path/to/file
+       |  mill benchmark.Benchmark 5,10 16,512 1000 string path/to/file
      """.stripMargin
 
   def validate(args: Array[String]): BenchmarkArgs = {
-    if (args.length != 6) {
+    if (args.length != 5) {
       throw new IllegalArgumentException(s"""Incorrect number of arguments\n\n""" + usage)
     }
 
@@ -62,11 +61,6 @@ object Args {
       }
     }
 
-    val validK = (k: String) => getInt(k) match {
-      case Some(v) if v > 0 => v
-      case _ => throw new IllegalArgumentException("k should be a positive integer")
-    }
-
     val validBufferSize = (bufferSize: String) => getInt(bufferSize) match {
       case Some(v) if v > 0 => v
       case _ => throw new IllegalArgumentException("buffer_size should be a positive integer")
@@ -82,15 +76,15 @@ object Args {
       else throw new IllegalArgumentException("File does not exist")
     }
 
-    val validators = List(validDepthRange, validWidthRange, validK, validBufferSize, validDataType, validFile)
+    val validators = List(validDepthRange, validWidthRange, validBufferSize, validDataType, validFile)
     val results = args.zip(validators).map { case (arg, validate) => Try(validate(arg)) }
 
     if (results.exists(_.isFailure)) {
       Left(results.filter(_.isFailure).map { case Failure(e) => e.getMessage }.toList)
     } else {
       val resultValues = results.map(_.get)
-      (resultValues(0), resultValues(1), resultValues(2), resultValues(3), resultValues(4), resultValues(5)) match {
-        case t: (List[Int], List[Int], Int, Int, String, String) => Right(BenchmarkArgs.tupled(t))
+      (resultValues(0), resultValues(1), resultValues(2), resultValues(3), resultValues(4)) match {
+        case t: (List[Int], List[Int], Int, String, String) => Right(BenchmarkArgs.tupled(t))
       }
     }
   }
