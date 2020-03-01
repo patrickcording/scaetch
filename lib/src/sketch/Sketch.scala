382d5aef8325d3a19ceb1b9169b474dda88c60b9
Patrick Cording <patrick.cording@datarobot.com>
2020-03-01 22:04:26 +0100

    Remove newline

--- lib/src/sketch/Sketch.scala
+++ lib/src/sketch/Sketch.scala
@@ -7,5 +7,4 @@ abstract class Sketch[A] extends Serializable {
   def add[T](elem: T)(implicit hash: HashFunctionSimulator[T]): A = add(elem, 1L)
   def estimate[T](elem: T)(implicit hash: HashFunctionSimulator[T]): Long
   def merge(other: A): A
-
 }
