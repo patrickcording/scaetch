a5471a879a8a7a658fb8748f47ec0c417f2040de
Patrick Cording <patrick.cording@datarobot.com>
2020-03-01 22:04:15 +0100

    Add convenience method for adding one element

--- lib/src/sketch/Sketch.scala
+++ lib/src/sketch/Sketch.scala
@@ -4,6 +4,8 @@ import sketch.hash.HashFunctionSimulator
 
 abstract class Sketch[A] extends Serializable {
   def add[T](elem: T, count: Long)(implicit hash: HashFunctionSimulator[T]): A
+  def add[T](elem: T)(implicit hash: HashFunctionSimulator[T]): A = add(elem, 1L)
   def estimate[T](elem: T)(implicit hash: HashFunctionSimulator[T]): Long
   def merge(other: A): A
+
 }
