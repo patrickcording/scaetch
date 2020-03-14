0e16d499cd8e2bfc748eb9c0c7bacb678f019ae9
Patrick Cording <patrick.cording@datarobot.com>
2020-03-14 19:26:03 +0100

    Add hash function simulator for Any

--- lib/src/sketch/hash/HashFunctionSimulator.scala
+++ lib/src/sketch/hash/HashFunctionSimulator.scala
@@ -7,9 +7,12 @@ trait HashFunctionSimulator[T] {
   protected var b = 0
 
   def set(x: T): Unit
+
   def hash(i: Int): Int = {
     a*i + b
   }
+
+  def getState: (Int, Int) = (a, b)
 }
 
 class LongHashFunctionSimulator(seed: Long) extends HashFunctionSimulator[Long] {
@@ -32,5 +35,27 @@ class StringHashFunctionSimulator(seed: Long) extends HashFunctionSimulator[Stri
     val v = h.hashChars(x)
     a = (v >>> 32).toInt
     b = (v & 0xFFFFFFFFL).toInt
+  }
+}
+
+class AnyHashFunctionSimulator(seed: Long) extends HashFunctionSimulator[Any] {
+  private val stringHashFunctionSimulator = new StringHashFunctionSimulator(seed)
+  private val longHashFunctionSimulator = new LongHashFunctionSimulator(seed)
+
+  private def setState(state: (Int, Int)) = {
+    a = state._1
+    b = state._2
+  }
+
+  override def set(x: Any): Unit = {
+    x match {
+      case xs: String =>
+        stringHashFunctionSimulator.set(xs)
+        setState(stringHashFunctionSimulator.getState)
+      case xl: Long =>
+        longHashFunctionSimulator.set(xl)
+        setState(longHashFunctionSimulator.getState)
+      case _ => throw new NotImplementedError("No hash function simulator found for type of input")
+    }
   }
 }
--- lib/src/sketch/hash/implicits.scala
+++ lib/src/sketch/hash/implicits.scala
@@ -3,4 +3,5 @@ package sketch.hash
 object implicits {
   implicit val longHashFunction = new LongHashFunctionSimulator(42)
   implicit val stringHashFunction = new StringHashFunctionSimulator(42)
+  implicit val anyHashFunction = new AnyHashFunctionSimulator(42)
 }
