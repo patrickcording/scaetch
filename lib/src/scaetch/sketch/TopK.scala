110c95264317e7a33861c0a7b5619a5330ff2ee6
Patrick Cording <patrick.cording@gmail.com>
2020-06-29 21:06:23 +0200

    Rename class to reflect the actual implemented algorithm

--- lib/src/scaetch/sketch/TopK.scala
+++ lib/src/scaetch/sketch/TopK.scala
@@ -4,10 +4,10 @@ import scaetch.ds.MaxList
 import scaetch.sketch.hash.HashFunctionSimulator
 
 
-class HeavyHitter[T](k: Int, sketch: Sketch) {
+class TopK[T](k: Int, sketch: Sketch) {
   private val topK = new MaxList[T](k)
 
-  def add(elem: T)(implicit hash: HashFunctionSimulator[T]): HeavyHitter[T] = {
+  def add(elem: T)(implicit hash: HashFunctionSimulator[T]): TopK[T] = {
     val elemCount = sketch.add(elem).estimate(elem)
     topK.add(elemCount, elem)
     this
